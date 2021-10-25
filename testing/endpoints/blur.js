14f9be48848a009ed41c1c73a3994f255b601331
Zerio <43386412+ZerioDev@users.noreply.github.com>
2021-10-25 12:08:05 +0200

    💡 Comments

--- testing/endpoints/blur.js
+++ testing/endpoints/blur.js
@@ -5,3 +5,12 @@ new Blur()
     .setLevel(5)
     .write('./img/blur.jpg')
     .build().then(console.log('Saved'))
+
+/*
+    .setImage() will be the picture used
+    .setLevel() will be the level used on the filter (can be between 1 and 20)
+    .write() to save the new picture in a file
+    .build() to get the result (return a buffer if needed)
+
+    If the number in .setLevel() is not between 1 and 20 the value will automatically be set to the number 5
+*/
--- testing/endpoints/greyscale.js
+++ testing/endpoints/greyscale.js
@@ -4,3 +4,9 @@ new Greyscale()
     .setImage('./img/avatar.jpg')
     .write('./img/greyscale.jpg')
     .build().then(console.log('Saved'))
+
+/*
+    .setImage() will be the picture used
+    .write() to save the new picture in a file
+    .build() to get the result (return a buffer if needed)
+*/
